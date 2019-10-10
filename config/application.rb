d2b9ca213ddcde0739b72ff1dc5c6bc4eca868be
Risa Kimura <Risa@parettokanrishanoMacBook-puro.local>
2019-10-10 15:24:07 +0900

    Chane to Japanese

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module App
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
